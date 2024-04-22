ec2dd5b4fe03a414e90e34b01112d14e18e168d8
KlintLee115 <54990359+KlintLee115@users.noreply.github.com>
2024-04-21 20:23:52 -0600

    t

--- LibraryManagementSystem/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/LibraryManagementSystem.AssemblyInfo.cs
+++ LibraryManagementSystem/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/LibraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+bbc9a855f56c2962eaa1c7392b10659092089ceb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5306a5de7752e9cf04895ca017b71b79e2c8f40a")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
